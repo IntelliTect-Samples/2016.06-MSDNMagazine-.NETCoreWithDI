63e63962b9f56d1d83ff56cb60f1fee879ee94f1
Mark Michaelis - Personal <Mark Michaelis - Personal>
2016-04-25 04:30:29 -0700

    Created create-gitsubmodules.ps1

--- ASPNETSampleWithDI/submodules/Create-GitSubmodules.ps1
+++ ASPNETSampleWithDI/submodules/Create-GitSubmodules.ps1
@@ -2,8 +2,13 @@ $currentLocation = Get-Location
 try {
 set-location $PSScriptRoot
 git submodule --quiet add -b "(1.0.0-rc1)" https://github.com/aspnet/DependencyInjection.git
+Set-Location DependencyInjection
+git checkout "1.0.0-rc1"
+Set-Location ..
 git submodule --quiet add -b "(1.0.0-rc1)" https://github.com/aspnet/Hosting.git
+Set-Location Hosting
+git checkout "1.0.0-rc1"
 }
 finally {
     Set-Location $currentLocation
-}dir
+}
