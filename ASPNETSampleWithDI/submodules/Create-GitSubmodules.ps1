83fc14a4a420a0c78977544ad084c103afd81407
Mark Michaelis - Personal <Mark Michaelis - Personal>
2016-04-27 12:55:05 -0700

    Refactored script (without testing - blush)

--- ASPNETSampleWithDI/submodules/Create-GitSubmodules.ps1
+++ ASPNETSampleWithDI/submodules/Create-GitSubmodules.ps1
@@ -1,13 +1,31 @@
+
+Function Add-Submodule {
+    param(
+        [Parameter(Position=1, Mandatory=$True, ValueFromPipeline=$True)][Url]$uri,
+        $branch
+    )
+    $currentLocation = Get-Location
+    try {
+        git submodule --quiet add -b "(1.0.0-rc1)" https://github.com/aspnet/DependencyInjection.git
+        $submoduleName = [IO.Path]::GetFileNameWithoutExtension( (([Uri]"$uri").AbsolutePath))
+        Set-Location $submoduleName
+        if($branch) {
+            git checkout $branch  # Arg, needed because sometime -b doesn't work for some uninvestigated reason.
+        }
+    }
+    finally {
+        Set-Location $currentLocation
+    }
+}
+
+#Warning: Refactored without testing. :)
+
 $currentLocation = Get-Location
 try {
-set-location $PSScriptRoot
-git submodule --quiet add -b "(1.0.0-rc1)" https://github.com/aspnet/DependencyInjection.git
-Set-Location DependencyInjection
-git checkout "1.0.0-rc1"
-Set-Location ..
-git submodule --quiet add -b "(1.0.0-rc1)" https://github.com/aspnet/Hosting.git
-Set-Location Hosting
-git checkout "1.0.0-rc1"
+    set-location $PSScriptRoot
+    "DependencyInjection","Hosting" | %{
+            Add-Submodule "https://github.com/aspnet/$_.git" "(1.0.0-rc1)"
+    }
 }
 finally {
     Set-Location $currentLocation
